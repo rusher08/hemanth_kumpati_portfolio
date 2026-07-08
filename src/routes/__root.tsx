61c0e49ecc008e160e2b073444c0cd33f11b8f9b
Hemanth Kumpati <132389730+rusher08@users.noreply.github.com>
2026-07-08 11:52:33 +0530

    speed insights

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import {
   Outlet,
   Link,
